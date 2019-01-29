d9c99430d6cd3bd32e7609b375c9040803836379
Alyson Turley <alyson.turley@gmail.com>
2019-01-28 19:34:06 -0600

    started slick implementation

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    autoplay: true,
+    speed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
